2c6cc12a9bfc81eba063492ffbd2fd450f942193
PierreRlld <p.rouillard@outlook.com>
2022-02-19 18:42:29 +0100

    sous tableau favoris

--- py scripts/profiles.py
+++ py scripts/profiles.py
@@ -15,43 +15,79 @@ class work():
         self.tags=self.features['tags']
 
         self.current=start                  # à quel scan on est
-    
-    def update(self,step=1):   #maj scan ; base step = 1
-        self.current=self.current + step
 
 #%%
 class profile():
 
-    def __init__(self,name): 
+    def __init__(self,name): # attribus de la classe profile 
         self.name=name
-        self.collec={}      # dictionnaire qui va stocker les lectures en cours + objet de type work
-    
-    def add_collec(self,title):
-        self.collec[title]=work(self.name,title)
+        self.collec={}      # dictionnaire qui va stocker les lectures en cours = objet de type work
+
+
+    def add_collec(self,title):                             
+        self.collec[title]=[work(self.name,title),False]    
+# Prends en paramètre le nom d'utilisateur !!
+# dictionnaire self.collec contient {nom oeuvre : [ <type work>, statut favoris] 
+# on précise pas start dans work(..) on suppose qu'on commence au scan 1
 
     def rem_collec(self,title):
         del self.collec[title]
         return None
-    
+
+    def fav(self,title,action):     # définition du status de favoris
+        if action=='add':
+            self.collec[title][1]=True
+        elif action=='del':
+            self.collec[title][1]=False
+
+
     def collecTab(self):
         #keys et values : opérations sur dictionnaire (les values sont des types work)
-        return pd.DataFrame({'Title':list(self.collec.keys()),'Current':[work.current for work in list(self.collec.values())]}) 
+        dic={'Title':list(self.collec.keys()), 'Current':[work[0].current for work in list(self.collec.values())], 'Favorite':[x[1] for x in list(self.collec.values())] }
+        return pd.DataFrame(dic) 
 
 
-#%%
-nobu=profile('Nobu')
-nobu.add_collec('One piece')
-nobu.collec['One piece'].update(10)
-nobu.add_collec('Naruto')
-#nobu.rem_collec('One piece')
-nobu.collecTab()
+    def favTab(self):
+        dic={'Title':[x for x in list(self.collec.keys()) if self.collec[x][1]==True], 'Current':[work[0].current for work in list(self.collec.values()) if work[1]==True]}
+        return pd.DataFrame(dic)
+
+# ================= #
+
+def Update(profile,title,n):    # définir à quel scan on est à partir du numéro
+    profile.collec[title][0].current=n
+
+def UpdateAdd(profile,title,step=1):    #maj scan avec un nombre de scan de lu ; base step = 1
+    profile.collec[title][0].current+=step
+
+
+
 #%%
 " =========================================================== "
 " ======================== TEST ============================= "
 " =========================================================== "
-nobu.collec['Naruto'].update(35)
+
+nobu=profile('Nobu')
+nobu.add_collec('One piece')
+#nobu.collec['One piece'].update(10)
+Update(nobu,'One piece',15)
+nobu.add_collec('Naruto')
+UpdateAdd(nobu,'Naruto',4)
+nobu.fav('Naruto','add')
+
+print('Favorite tab\n')
+nobu.favTab()
+#nobu.rem_collec('One piece')
+
+#%%
+print('Total tab\n')
 nobu.collecTab()
 
+#%%
+" =========================================================== "
+" ======================== OLD TEST ============================= "
+" =========================================================== "
+nobu.collec['Naruto'].update(35)
+nobu.collecTab()
 #%%
 # Test class work avec dépendance au nom d'utilisateur 
 
