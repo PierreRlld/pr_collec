61e562f68137007653cc42bc9aa0950729366a96
PierreRlld <p.rouillard@outlook.com>
2022-02-21 21:19:12 +0100

    clean script

--- py scripts/profiles.py
+++ py scripts/profiles.py
@@ -26,9 +26,9 @@ class profile():
 
     def add_collec(self,title):                             
         self.collec[title]=[work(self.name,title),False]    
-# Prends en paramètre le nom d'utilisateur !!
-# dictionnaire self.collec contient {nom oeuvre : [ <type work>, statut favoris] 
-# on précise pas start dans work(..) on suppose qu'on commence au scan 1
+    # Prends en paramètre le nom d'utilisateur !!
+    # dictionnaire self.collec contient {nom oeuvre : [ <type work>, statut favoris] 
+    # on précise pas start dans work(..) on suppose qu'on commence au scan 1
 
     def rem_collec(self,title):
         del self.collec[title]
@@ -40,6 +40,7 @@ class profile():
         elif action=='del':
             self.collec[title][1]=False
 
+    # DataFrames 
 
     def collecTab(self):
         #keys et values : opérations sur dictionnaire (les values sont des types work)
@@ -51,14 +52,13 @@ class profile():
         dic={'Title':[x for x in list(self.collec.keys()) if self.collec[x][1]==True], 'Current':[work[0].current for work in list(self.collec.values()) if work[1]==True]}
         return pd.DataFrame(dic)
 
-# Fonctions pour update scan en fonction du profile #
 
-def Update(profile,title,n):    # définir à quel scan on est à partir du numéro
+# Fonctions pour update scan en fonction du profil
+def Update(profile,title,n):                # définir à quel scan on est à partir du numéro
     profile.collec[title][0].current=n
 
-def UpdateAdd(profile,title,step=1):    #maj scan avec un nombre de scan de lu ; base step = 1
+def UpdateAdd(profile,title,step=1):        #maj scan avec un nombre de scan de lu ; base step = 1
     profile.collec[title][0].current+=step
-
 
 
 #%%
@@ -82,25 +82,8 @@ nobu.collecTab()
 #%%
 print('Favorite tab\n')
 nobu.favTab()
+#print(type(nobu))
 
-#%%
-" =========================================================== "
-" ======================== OLD TEST ============================= "
-" =========================================================== "
-nobu.collec['Naruto'].update(35)
-nobu.collecTab()
-#%%
-# Test class work avec dépendance au nom d'utilisateur 
-
-op=work('Nobu','One piece')
-op.update(10)
-op.current
-
-op2=work('Pierre','One piece')
-op2.update(20)
-op2.current
-
-print(op.current,op2.current)
 #%%
 test1={'One piece':work('nobu','One piEce'),'Naruto':work('nobu','naruto')}
 dftest=pd.DataFrame({'Title':list(test1.keys()), 'Current':[work.current for work in list(test1.values())] })
